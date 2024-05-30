5e0d3c49fae1c89965eb119c6385a36fbabee0b6
Philip Penchev <34584602+philippenchev1@users.noreply.github.com>
2024-05-30 15:37:59 +0300

    pathfinder

--- pathfinder/src/main/java/bg/softuni/pathfinder/web/UsersController.java
+++ pathfinder/src/main/java/bg/softuni/pathfinder/web/UsersController.java
@@ -1,18 +1,49 @@
 package bg.softuni.pathfinder.web;
 
+import bg.softuni.pathfinder.model.dtos.UserRegisterDto;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.net.http.HttpClient;
 
 @Controller
 public class UsersController {
 
+
+
+    @GetMapping("/users/register")
+    public String register(Model model) {
+        model.addAttribute("registerData", new UserRegisterDto());
+
+        return "register";
+    }
+
+
+    @PostMapping("/users/register")
+    public String doRegister(@Valid UserRegisterDto data,
+                             BindingResult bindingResult,
+                             RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("registerData", data);
+
+
+            // handle errors
+            return "register";
+        }
+
+
+        // register user
+        return "redirect:/users/login";
+    }
+
     @GetMapping("/users/login")
     public String login(){
         return "login";
-    }
-
-    @GetMapping("/users/register")
-    public String register(){
-        return "register";
     }
 }
